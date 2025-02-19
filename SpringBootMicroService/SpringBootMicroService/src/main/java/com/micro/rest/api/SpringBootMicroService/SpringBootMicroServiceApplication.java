81ffd0f3056331f5f09c9d3c2fa68bdefe8fb4cb
RojaSarathy <rojakannanmsc@gmail.com>
2025-02-19 14:51:34 +0530

    Add files via upload

---
+++ SpringBootMicroService/SpringBootMicroService/src/main/java/com/micro/rest/api/SpringBootMicroService/SpringBootMicroServiceApplication.java
@@ -0,0 +1,16 @@
+package com.micro.rest.api.SpringBootMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroServiceApplication.class, args);
+	}
+
+}
---
+++ SpringBootMicroService/SpringBootMicroService/src/test/java/com/micro/rest/api/SpringBootMicroService/SpringBootMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.rest.api.SpringBootMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
